4f9c49ebc3ede71166f2f89415aecfa9e9fd8293
Gibson Hurst <102323807+GibsonHurst@users.noreply.github.com>
2024-04-03 19:55:26 -0400

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,17 @@
+print("Marvel Movie Character Creator")
+print()
+spider = input("Do you like 'hanging around'? (Y/N) ")
+if spider == "Y":
+	print("Then you must be Spider-man")
+else:
+	print("Then you're not Spider-man")
+	korg = input("Do you have a 'gravelly' voice? (Y/N) ")
+	if korg == "Y":
+		print("Then you must be Korg")
+	else:
+		print("Then you're not Korg")
+		cap = input("Do you often feel 'Marvelous'? (Y/N) ")
+		if cap == "Y":
+			print("Aha! You're Captain Marvel! Hi!")
+		else:
+			print("You're not a Marvel Character")
