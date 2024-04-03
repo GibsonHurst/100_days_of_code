2210e08c7ad33ae334a7daccdfce29929d288331
Gibson Hurst <102323807+GibsonHurst@users.noreply.github.com>
2024-04-03 19:39:45 -0400

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,12 @@
+print("""Welcome to your adventure simulator. 
+I am going to ask you a bunch of questions and 
+then create an epic story with you as the star!""")
+print()
+name = input('What is your name? ')
+enemy = input('What is your worst enemy’s name? ')
+super = input('What is your superpower? ')
+live = input("Where do you live? ")
+food = input("What is your favorite food? ")
+print()
+print("Hello", name + "! Your ability to", super, "will make sure you never have to look at", enemy, "again.")
+print("Go eat",food, "as you walk down the streets of", live, "and use", super, "for good and not evil!")
