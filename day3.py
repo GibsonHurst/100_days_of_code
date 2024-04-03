b11d4cf07a79520aee7a4817341050ef5bcd33cc
Gibson Hurst <102323807+GibsonHurst@users.noreply.github.com>
2024-04-03 19:26:32 -0400

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,9 @@
+food = input("Name a food: ")
+plant = input("Name a plant: ")
+cooking = input("Name a method of cooking: ")
+burned = input("Name a word that describes burned food: ")
+item = input("Name a household item: ")
+print()
+print("Recipe")
+print("Description: ", cooking, food, "with", burned, plant, "on a bed of", item)
+
