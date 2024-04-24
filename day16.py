84ba5db5d9537bbc44bb32d567be40c1dcd0b634
Gibson Hurst <102323807+GibsonHurst@users.noreply.github.com>
2024-04-24 19:06:33 -0400

    Add files via upload

---
+++ day16.py
@@ -0,0 +1,9 @@
+while True:
+	print("Lyric: Never gonna ______ you up.")
+	word = input("Type in the blank word and see if you are as cool as me: ")
+	if word == "give":
+		break
+	else:
+		print("Try Again!")
+print("Correct")
+
